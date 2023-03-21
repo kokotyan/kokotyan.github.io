81cdf1c0995292691469374b0a3644df627b36d2
kokotyan <kokochi.0207@gmail.com>
2023-03-21 12:02:22 +0900

    ヘッダーのリンクにちゃんと飛べるようにした

--- script.js
+++ script.js
@@ -25,3 +25,42 @@ const menuLines = document.querySelectorAll('.menu-line');
 menuIcon.addEventListener('click', () => {
     menuLines.forEach(line => line.classList.toggle('menu-line-active'));
 });
+
+$(function () {
+    /*==================================
+    スマホメニュー
+    ===================================*/
+    // ハンバーガーメニューのクリックイベント
+    // 解説は、「中級編:ストアサイト(インテリア)」参照
+    $('.toggle_btn').on('click', function () {
+        if ($('#header').hasClass('open')) {
+            $('#header').removeClass('open');
+        } else {
+            $('#header').addClass('open');
+        }
+    });
+    // #maskのエリアをクリックした時にメニューを閉じる
+    $('#mask').on('click', function () {
+        $('#header').removeClass('open');
+    });
+
+    // リンクをクリックしたときにメニューを閉じる
+    //ミスタイプ ('#navi a').on('click', function () {
+    //     $('#header').removeClass('open')
+    // });
+
+    /*================================================
+    スムーススクロール
+    =================================================*/
+    // ページ内リンクのイベント
+    $('a[href^="#"]').click(function () {
+        // リンクを取得
+        let href = $(this).attr("href");
+        // ジャンプ先のid名をセット
+        let target = $(href == "#" || href == "" ? 'html' : href);
+        // トップからジャンプ先の要素までの距離を取得
+        let position = target.offset().top;
+        // animateでスムーススクロールを行う
+        // 600はスクロール運動で単位はミリ秒
+        $("html, body").animate({ scrollTop: position }, 600, "swing");
+        return false;
