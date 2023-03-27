5c618a22cf2db26d2e939d8fb45e8122703ae686
kokotyan <kokochi.0207@gmail.com>
2023-03-27 15:02:22 +0900

    ハンバーガーメニューの追加。 まだレスポンシブにできず

--- script.js
+++ script.js
@@ -11,56 +11,36 @@ function fadeAnime() {
     });
 }
 
-$(window).scroll(function () {
-    fadeAnime();
-});
+//ハンバーガーメニュー用はじめ
 
-$(window).on('load', function () {
-    fadeAnime();
-});
+const MenuContainer = document.querySelector('.menu_container');
+const MenuOpen = document.querySelector('.menu_open');
+const MEnuClose = document.querySelector('.menu_close');
 
-const menuIcon = document.querySelector('.menu-icon');
-const menuLines = document.querySelectorAll('.menu-line');
+const SPMenu = document.querySelector('.sample_sp_menu');
+	MenuContainer.addEventListener('click', () => {
+        MenuOpen.classList.toggle('active');
+        MEnuClose.classList.toggle('active');
+        SPMenu.classList.toggle('active');
+	});
 
-menuIcon.addEventListener('click', () => {
-    menuLines.forEach(line => line.classList.toggle('menu-line-active'));
-});
+    //ハンバーガーメニュー終わり
 
-$(function () {
-    /*==================================
-    スマホメニュー
-    ===================================*/
-    // ハンバーガーメニューのクリックイベント
-    // 解説は、「中級編:ストアサイト(インテリア)」参照
-    $('.toggle_btn').on('click', function () {
-        if ($('#header').hasClass('open')) {
-            $('#header').removeClass('open');
-        } else {
-            $('#header').addClass('open');
-        }
-    });
-    // #maskのエリアをクリックした時にメニューを閉じる
-    $('#mask').on('click', function () {
-        $('#header').removeClass('open');
-    });
+    $('ハンバーガーアイコンのセレクター').click(function() {
+        $('nav').slideToggle();
+        });
 
-    // リンクをクリックしたときにメニューを閉じる
-    //ミスタイプ ('#navi a').on('click', function () {
-    //     $('#header').removeClass('open')
-    // });
+// $(window).scroll(function () {
+//     fadeAnime();
+// });
 
-    /*================================================
-    スムーススクロール
-    =================================================*/
-    // ページ内リンクのイベント
-    $('a[href^="#"]').click(function () {
-        // リンクを取得
-        let href = $(this).attr("href");
-        // ジャンプ先のid名をセット
-        let target = $(href == "#" || href == "" ? 'html' : href);
-        // トップからジャンプ先の要素までの距離を取得
-        let position = target.offset().top;
-        // animateでスムーススクロールを行う
-        // 600はスクロール運動で単位はミリ秒
-        $("html, body").animate({ scrollTop: position }, 600, "swing");
-        return false;
+// $(window).on('load', function () {
+//     fadeAnime();
+// });
+
+// const menuIcon = document.querySelector('.menu-icon');
+// const menuLines = document.querySelectorAll('.menu-line');
+
+// menuIcon.addEventListener('click', () => {
+//     menuLines.forEach(line => line.classList.toggle('menu-line-active'));
+// });
