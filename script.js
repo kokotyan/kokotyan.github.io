8683cfbe855f7b459fdcba62187b1b299c04a69b
kokotyan <kokochi.0207@gmail.com>
2023-03-31 11:06:29 +0900

    いらないコードを省きました

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//画面のアニメーション
+
 function fadeAnime() {
     $('.fadeUpTrigger').each(function () {
         var elemPos = $(this).offset().top - 50;
@@ -18,29 +20,10 @@ const MenuOpen = document.querySelector('.menu_open');
 const MEnuClose = document.querySelector('.menu_close');
 
 const SPMenu = document.querySelector('.sample_sp_menu');
-	MenuContainer.addEventListener('click', () => {
-        MenuOpen.classList.toggle('active');
-        MEnuClose.classList.toggle('active');
-        SPMenu.classList.toggle('active');
-	});
+MenuContainer.addEventListener('click', () => {
+    MenuOpen.classList.toggle('active');
+    MEnuClose.classList.toggle('active');
+    SPMenu.classList.toggle('active');
+});
 
-    //ハンバーガーメニュー終わり
-
-    $('ハンバーガーアイコンのセレクター').click(function() {
-        $('nav').slideToggle();
-        });
-
-// $(window).scroll(function () {
-//     fadeAnime();
-// });
-
-// $(window).on('load', function () {
-//     fadeAnime();
-// });
-
-// const menuIcon = document.querySelector('.menu-icon');
-// const menuLines = document.querySelectorAll('.menu-line');
-
-// menuIcon.addEventListener('click', () => {
-//     menuLines.forEach(line => line.classList.toggle('menu-line-active'));
-// });
+//ハンバーガーメニュー終わり
