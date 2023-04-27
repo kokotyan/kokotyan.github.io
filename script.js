b1f1ee39606b43bac2de19c0d7398f7d91987734
kokotyan <kokochi.0207@gmail.com>
2023-04-27 14:01:57 +0900

    footerにsns欄の追加及びskill欄も追加。

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 $(function(){
-    $("#drawer_toggle").click(function(){
-        $(this).toggleClass("open");
-        $("#gloval_nav").toggleClass("sp_open");
-    });
+  $("#drawer_toggle").click(function(){
+      $(this).toggleClass("open");
+      $("#gloval_nav").toggleClass("sp_open");
+  });
+
+  // ナビゲーションのリンクがクリックされたら、ハンバーガーメニューを閉じる
+  $("#gloval_nav a").click(function() {
+      $("#drawer_toggle").removeClass("open");
+      $("#gloval_nav").removeClass("sp_open");
+  });
 });
