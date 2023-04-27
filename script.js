8bec1a986df227d26d238a5699cc083922bb818b
kokotyan <kokochi.0207@gmail.com>
2023-04-27 11:01:11 +0900

    思い通りのハンバーガーの完成、メディアクエリ対応！

--- script.js
+++ script.js
@@ -1,37 +1,6 @@
-//画面のアニメーション
-
-// function fadeAnime() {
-//     $('.fadeUpTrigger').each(function () {
-//         var elemPos = $(this).offset().top - 50;
-//         var scroll = $(window).scrollTop();
-//         var windowHeight = $(window).height();
-//         if (scroll >= elemPos - windowHeight) {
-//             $(this).addClass('fadeUp');
-//         } else {
-//             $(this).removeClass('fadeUp');
-//         }
-//     });
-// }
-
-//ハンバーガーメニュー用はじめ
-
-const MenuContainer = document.querySelector('.menu_container');
-const MenuOpen = document.querySelector('.menu_open');
-const MEnuClose = document.querySelector('.menu_close');
-
-const SPMenu = document.querySelector('.sample_sp_menu');
-MenuContainer.addEventListener('click', () => {
-    MenuOpen.classList.toggle('active');
-    MEnuClose.classList.toggle('active');
-    SPMenu.classList.toggle('active');
+$(function(){
+    $("#drawer_toggle").click(function(){
+        $(this).toggleClass("open");
+        $("#gloval_nav").toggleClass("sp_open");
+    });
 });
-
-//考えた末chatGPTを利用
-SPMenu.addEventListener('click', () => {
-    MenuOpen.classList.remove('active');
-    MEnuClose.classList.add('active');
-    SPMenu.classList.remove('active');
-});
-
-
-//ハンバーガーメニュー終わり
