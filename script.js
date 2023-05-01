dc7469490e729b96a6bf05c6c77114e440354b85
kokotyan <kokochi.0207@gmail.com>
2023-05-01 13:15:53 +0900

    hobby欄にslick.jsにてアニメーションを追加

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-$(function(){
-  $("#drawer_toggle").click(function(){
-      $(this).toggleClass("open");
-      $("#gloval_nav").toggleClass("sp_open");
+$(function () {
+  $("#drawer_toggle").click(function () {
+    $(this).toggleClass("open");
+    $("#gloval_nav").toggleClass("sp_open");
   });
 
   // ナビゲーションのリンクがクリックされたら、ハンバーガーメニューを閉じる
-  $("#gloval_nav a").click(function() {
-      $("#drawer_toggle").removeClass("open");
-      $("#gloval_nav").removeClass("sp_open");
+  $("#gloval_nav a").click(function () {
+    $("#drawer_toggle").removeClass("open");
+    $("#gloval_nav").removeClass("sp_open");
   });
 });
+
+$("#main-slider").slick({
+  autoplay: true,
+  autoplaySpeed: 6000,
+  infinite: true,
+  fade: true,
+  asNavFor: "#text-slider",
+  pauseOnFocus: false,
+  pauseOnHover: false,
+  pauseOnDotsHover: false,
+});
+
+$("#text-slider").slick({
+  draggble: false,
+  arrows: false,
+});
+
+
+$('#main-slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+  $('#main-slider').slick('slickPlay');
+  });
