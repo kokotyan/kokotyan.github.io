f5a56de6b918da42619ac105da799023c227fad7
kokotyan <kokochi.0207@gmail.com>
2023-05-15 11:30:16 +0900

    リファクタリングとリンクで飛んだ時にhobbyが見切れてしまうのを改善

--- script.js
+++ script.js
@@ -11,6 +11,35 @@ $(function () {
   });
 });
 
+  $(document).ready(function() {
+    // セクションの高さを計算する
+    var sections = $("section");
+    var nav = $("#global_nav");
+
+    $(window).on("scroll", function() {
+      var curPos = $(this).scrollTop();
+      sections.each(function() {
+        var top = $(this).offset().top - 50; // ヘッダーの高さを考慮する
+        var bottom = top + $(this).outerHeight();
+        if (curPos >= top && curPos <= bottom) {
+          nav.find("a").removeClass("active");
+          sections.removeClass("active");
+          $(this).addClass("active");
+          nav.find('a[href="#' + $(this).attr("id") + '"]').addClass("active");
+        }
+      });
+    });
+
+    // スムーススクロールの実装
+    $("a[href^='#']").click(function(e) {
+      e.preventDefault();
+      var target = $(this).attr("href");
+      $("html, body").animate({
+        scrollTop: $(target).offset().top - 50 // ヘッダーの高さを考慮する
+      }, 500);
+    });
+  });
+
 $("#main-slider").slick({
   autoplay: true,
   autoplaySpeed: 1500,
