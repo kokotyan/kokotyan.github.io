512cf17c0885350d952712b4cacc449e27a09576
kokotyan <kokochi.0207@gmail.com>
2023-05-15 13:47:41 +0900

    画面サイズが小さい時のnewsの文字が崩れるのを修正しました

--- script.js
+++ script.js
@@ -1,62 +1,82 @@
-$(function () {
-  $("#drawer_toggle").click(function () {
+$(function() {
+  $("#drawer_toggle").click(function() {
     $(this).toggleClass("open");
-    $("#gloval_nav").toggleClass("sp_open");
+    $("#global_nav").toggleClass("sp_open");
   });
 
-  // ナビゲーションのリンクがクリックされたら、ハンバーガーメニューを閉じる
-  $("#gloval_nav a").click(function () {
+  $("#global_nav a").click(function() {
     $("#drawer_toggle").removeClass("open");
-    $("#gloval_nav").removeClass("sp_open");
+    $("#global_nav").removeClass("sp_open");
   });
-});
+
+  $("#drawer_toggle").click(function() {
+    $(this).toggleClass("active");
+    $("#global_nav").toggleClass("active");
+  });
+
+  $("#global_nav a").click(function() {
+    $("#drawer_toggle").removeClass("active");
+    $("#global_nav").removeClass("active");
+  });
 
   $(document).ready(function() {
-    // セクションの高さを計算する
     var sections = $("section");
     var nav = $("#global_nav");
+    var headerHeight = $("header").outerHeight();
+
+    // スクロールイベントの最適化
+    var sectionPositions = [];
+    sections.each(function() {
+      sectionPositions.push($(this).offset().top - headerHeight);
+    });
 
     $(window).on("scroll", function() {
       var curPos = $(this).scrollTop();
-      sections.each(function() {
-        var top = $(this).offset().top - 50; // ヘッダーの高さを考慮する
-        var bottom = top + $(this).outerHeight();
-        if (curPos >= top && curPos <= bottom) {
-          nav.find("a").removeClass("active");
-          sections.removeClass("active");
-          $(this).addClass("active");
-          nav.find('a[href="#' + $(this).attr("id") + '"]').addClass("active");
+      var activeSectionIndex = -1;
+
+      for (var i = 0; i < sectionPositions.length; i++) {
+        if (curPos >= sectionPositions[i]) {
+          activeSectionIndex = i;
         }
-      });
+      }
+
+      nav.find("a").removeClass("active");
+      sections.removeClass("active");
+
+      if (activeSectionIndex !== -1) {
+        sections.eq(activeSectionIndex).addClass("active");
+        nav.find('a[href="#' + sections.eq(activeSectionIndex).attr("id") + '"]').addClass("active");
+      }
     });
-    
+
     // スムーススクロールの実装
     $("a[href^='#']").click(function(e) {
       e.preventDefault();
       var target = $(this).attr("href");
-      $("html, body").animate({
-        scrollTop: $(target).offset().top - 50 // ヘッダーの高さを考慮する
-      }, 500);
+      document.querySelector(target).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+      });
     });
   });
 
-$("#main-slider").slick({
-  autoplay: true,
-  autoplaySpeed: 1500,
-  infinite: true,
-  fade: true,
-  asNavFor: "#text-slider",
-  pauseOnFocus: false,
-  pauseOnHover: false,
-  pauseOnDotsHover: false,
-});
-
-$("#text-slider").slick({
-  draggble: false,
-  arrows: false,
-});
-
-
-$('#main-slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
-  $('#main-slider').slick('slickPlay');
+  $("#main-slider").slick({
+    autoplay: true,
+    autoplaySpeed: 1500,
+    infinite: true,
+    fade: true,
+    asNavFor: "#text-slider",
+    pauseOnFocus: false,
+    pauseOnHover: false,
   });
+
+  $("#text-slider").slick({
+    draggable: false,
+    arrows: false,
+  });
+
+  $("#main-slider").on("touchmove", function(event, slick, currentSlide, nextSlide) {
+    $("#main-slider").slick("slickPlay");
+  });
+});
